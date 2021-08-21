84e3b9ac177976daaf3e0918a8cdf257ef62476f
Martin Letis <mletis@google.com>
2021-08-21 21:34:54 +1000

    Working map with static token

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import flask
+import urllib
+
+# https://www.nsw.gov.au/media-releases/covid-19-restrictions-tightened-across-greater-sydney
+_10K_AFTER = 1625814000
+
+# https://www.nsw.gov.au/media-releases/increased-fines-test-and-isolate-payments-and-new-compliance-measures-as-nsw-battles
+_10K_BEFORE = _5K_AFTER = 1629036060
 
 app = flask.Flask(__name__)
 
 
-@app.route("/")
-def root():
-  return "Hello World!\n"
+@app.route('/json')
+def json():
+  after = flask.request.args.get('after')
+  before = flask.request.args.get('before')
+
+  params = {}
+  if (after):
+    params['after'] = after
+  if (before):
+    params['before'] = before
+    
+  activitiesUrl = urllib.parse.urlparse(
+      'https://www.strava.com/api/v3/athlete/activities')
+  activitiesUrl = activitiesUrl._replace(query=urllib.parse.urlencode(params))
+
+  request = urllib.request.Request(activitiesUrl.geturl(), headers = {
+    'Authorization': 'Bearer 11e54edafa0c6b22357eda56c898489d85ec8882'
+  })
+  with urllib.request.urlopen(request) as response:
+    return response.read()
 
 
-if __name__ == "__main__":
-  app.run(host="localhost", port=8080, debug=True)
+if __name__ == '__main__':
+  app.run(host='localhost', port=8080, debug=True)
---
+++ static/map.js
@@ -0,0 +1,52 @@
+function map(after, before, radius, zoom) {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: zoom,
+  });
+  var geocoder = new google.maps.Geocoder();
+
+  geocoder.geocode({'address': '149-197 Pyrmont Street, Pyrmont NSW 2009, Australia'}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+      var circle = new google.maps.Circle({
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.4,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.05,
+        map,
+        center: results[0].geometry.location,
+        radius: radius,
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+  
+  var colors = {
+    'Walk': '#0000FF',
+    'Ride': '#FF0000',
+    'Hike': '#00FF00',
+  };
+  
+  var activitiesUrl = new URL('/json', window.location.origin);
+  if (after) {
+    activitiesUrl.searchParams.append('after', after);
+  }
+  if (before) {
+    activitiesUrl.searchParams.append('before', before);
+  }
+  fetch(activitiesUrl)
+  .then(response => response.json())
+  .then(activities => activities.forEach(activity => new google.maps.Polyline({
+    path: google.maps.geometry.encoding.decodePath(activity.map.summary_polyline),
+    strokeColor: colors[activity.type],
+    strokeOpacity: 0.4,
+    strokeWeight: 6,
+    map: map
+  })));
+}
+
