188d5210fe7b56a9f28d556b6cba907271444cb0
Martin Letis <mletis@google.com>
2021-08-22 10:25:27 +1000

    API_KEY environment variables

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
 import json
+import os
 import time
 import urllib
 from google.cloud import datastore
@@ -10,24 +11,18 @@ _10K_AFTER = 1625814000
 # https://www.nsw.gov.au/media-releases/increased-fines-test-and-isolate-payments-and-new-compliance-measures-as-nsw-battles
 _10K_BEFORE = _5K_AFTER = 1629036060
 
-CLIENT_ID = "69382"
-CLIENT_SECRET = "5bcd3a399f4c849a2ffadf249eccecabbbaddca9"
-
 app = flask.Flask(__name__)
 datastore_client = datastore.Client()
 
 
-def refresh_token(token):
-  data = urllib.parse.urlencode({
-      "client_id": CLIENT_ID,
-      "client_secret": CLIENT_SECRET,
-      "grant_type": "refresh_token",
-      "refresh_token": token["refresh_token"],
-  }).encode("ascii")
-  with urllib.request.urlopen(
-      "https://www.strava.com/api/v3/oauth/token", data=data) as response:
-    token.update(json.loads(response.read()))
-    datastore_client.put(token)
+@app.route("/5k")
+def five():
+  return render(after=_5K_AFTER, before="undefined", radius=5000, zoom=13)
+
+
+@app.route("/10k")
+def ten():
+  return render(after=_10K_AFTER, before=_10K_BEFORE, radius=10000, zoom=12)
 
 
 @app.route("/activities")
@@ -52,11 +47,45 @@ def activities():
 
   if time.time() > token["expires_at"]:
     refresh_token(token)
-  
+
   headers = {"Authorization": "Bearer %s" % token["access_token"]}
   request = urllib.request.Request(activitiesUrl.geturl(), headers=headers)
   with urllib.request.urlopen(request) as response:
     return response.read()
+
+
+def refresh_token(token):
+  client_id = os.getenv("CLIENT_ID")
+  if client_id is None:
+    flask.abort(503, "Invalid CLIENT_ID")
+
+  client_secret = os.getenv("CLIENT_SECRET")
+  if client_secret is None:
+    flask.abort(503, "Invalid CLIENT_SECRET")
+
+  data = urllib.parse.urlencode({
+      "client_id": client_id,
+      "client_secret": client_secret,
+      "grant_type": "refresh_token",
+      "refresh_token": token["refresh_token"],
+  }).encode("ascii")
+  with urllib.request.urlopen(
+      "https://www.strava.com/api/v3/oauth/token", data=data) as response:
+    token.update(json.loads(response.read()))
+    datastore_client.put(token)
+
+
+def render(after, before, radius, zoom):
+  api_key = os.getenv("API_KEY")
+  if api_key is None:
+    flask.abort(503, "Invalid API_KEY")
+  return flask.render_template(
+      "map.html",
+      after=after,
+      before=before,
+      api_key=api_key,
+      radius=radius,
+      zoom=zoom)
 
 
 if __name__ == "__main__":
